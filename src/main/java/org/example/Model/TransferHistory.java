ca72920cb442615ded05aa90c11f55cca15bb1f1
Sheddy00 <hei.sheddy.2@gmail.com>
2023-12-08 19:07:54 +0400

    feat :update transferHistory

--- src/main/java/org/example/Model/TransferHistory.java
+++ src/main/java/org/example/Model/TransferHistory.java
@@ -1,4 +1,20 @@
 package org.example.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class TransferHistory {
+    private int id;
+    private int debitTransactionId;
+    private int creditTransactionId;
+    private double amount;
+    private Date transferDate;
+
 }
