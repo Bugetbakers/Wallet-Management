94c806796e00cf03cf28339656aa2c0cc9791dc8
Sheddy00 <hei.sheddy.2@gmail.com>
2023-12-08 19:06:23 +0400

    feat : currency value

--- src/main/java/org/example/Model/CurrencyValue.java
+++ src/main/java/org/example/Model/CurrencyValue.java
@@ -1,4 +1,18 @@
 package org.example.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CurrencyValue {
+    private int id;
+    private int baseCurrencyId;
+    private int targetCurrencyId;
+    private double value;
+    private Date date;
 }
