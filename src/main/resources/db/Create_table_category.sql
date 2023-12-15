e775254535d0ec6f691eaf01ee9253300a2db524
gitdeverstein <hei.andy.8@gmail.com>
2023-12-15 15:02:27 +0300

    feat: add category table in the sql

---
+++ src/main/resources/db/Create_table_category.sql
@@ -0,0 +1,14 @@
+INSERT INTO categories (name) VALUES
+      ('Food and Drink'),
+      ('Transportation'),
+      ('Shopping'),
+      ('Bills and Utilities'),
+      ('Entertainment'),
+      ('Health and Fitness'),
+      ('Travel'),
+      ('Education'),
+      ('Personal Care'),
+      ('Gifts and Donations'),
+      ('Home and Rent'),
+      ('Investments'),
+      ('Income');
