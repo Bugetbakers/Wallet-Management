d6eedd14ed0f880cf2feaf6de4d0be21d41d7fa0
gitdeverstein <hei.andy.8@gmail.com>
2024-01-10 10:28:59 +0300

    fix: debugging Create_table_account.sql

--- src/main/resources/db/Create_table_account.sql
+++ src/main/resources/db/Create_table_account.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS account(
-                                      id SERIAL PRIMARY KEY,
-                                      name VARCHAR(200) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
     balance NUMERIC(15,2) NOT NULL,
     transactionId INT REFERENCES transaction(id),
     currency INT REFERENCES currency(id),
-    type VARCHAR(100) CHECK (type IN ('Bank', 'Cash', 'MobileMoney')) NOT NULL
+    type VARCHAR(100) CHECK (type IN ('Bank', 'Cash', 'MobileMoney')) NOT NULL,
+    current_date_time timestamp default now()
     );
 
 ALTER SEQUENCE account_id_seq RESTART WITH 1;
