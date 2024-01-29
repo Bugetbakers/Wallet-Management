06534153caec9c48621d0c7449e661e0c6149bb5
Sheddy00 <hei.sheddy.2@gmail.com>
2024-01-29 21:22:56 +0100

    feat: add new class TransactionMapper for mapper methode

--- src/main/java/org/example/dao/TransactionMapper.java
+++ src/main/java/org/example/dao/TransactionMapper.java
@@ -1,2 +1,23 @@
-package org.example.dao;public class TransactionMapper {
+package org.example.dao;
+
+import org.example.model.Transaction;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionMapper extends reflectMapper<Transaction>{
+    public List<Transaction> mapResultsetToList(ResultSet resultSet) {
+        List<Transaction> transactions = new ArrayList<>();
+        try {
+            while (resultSet.next()) {
+                Transaction transaction = map(resultSet, Transaction.class);
+                transactions.add(transaction);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return transactions;
+    }
 }
