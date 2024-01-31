df4261d47f90b011db0f395dd4ab01c4132fc543
gitdeverstein <hei.andy.8@gmail.com>
2024-01-31 12:06:01 +0300

    refactor: modification of CrudOperationReflect.java

--- src/main/java/org/example/dao/CrudOperationReflect.java
+++ src/main/java/org/example/dao/CrudOperationReflect.java
@@ -13,6 +13,11 @@ public abstract class CrudOperationReflect<T> {
     public CrudOperationReflect(Connection connection){
         this.connection = connection;
     }
+    protected abstract String getTableName();
+    protected abstract String getInsertColumns();
+    protected abstract String getInsertValues();
+    protected abstract T mapResultSetToObject(ResultSet resultSet) throws SQLException, IllegalAccessException;
+    protected  abstract void setPreparedStatementParameters(PreparedStatement preparedStatement, T object) throws SQLException;
     public List<T> findAll(){
         List<T> result = new ArrayList<>();
         try{
@@ -23,10 +28,27 @@ public abstract class CrudOperationReflect<T> {
                 throw new RuntimeException(e);
             }
         }catch (Exception e){
-            throw  new RuntimeException(e);
         }
         return result;
     }
+
+    public T findById(int id) {
+        try {
+            String sql = "SELECT * FROM " + getTableName() + " WHERE id = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setInt(1, id);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                    return mapResultSetToObject(resultSet);
+                } else {
+                    return null; // Or throw an exception if preferred
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public T save(T object){
         try{
             String sql = "Insert into " +getTableName() + " " + getInsertColumns() + " Values" + getInsertValues();
@@ -39,9 +61,4 @@ public abstract class CrudOperationReflect<T> {
         }
         return object;
     }
-    protected abstract String getTableName();
-    protected abstract String getInsertColumns();
-    protected abstract String getInsertValues();
-    protected abstract T mapResultSetToObject(ResultSet resultSet) throws SQLException, IllegalAccessException;
-    protected  abstract T setPreparedStatementParameters(PreparedStatement preparedStatement, T object) throws SQLException;
 }
