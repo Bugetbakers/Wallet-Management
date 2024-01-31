bd905821ba0923fc133b6b5fed53573bc3a82edd
gitdeverstein <hei.andy.8@gmail.com>
2024-01-31 10:29:50 +0300

    refactor: modification of TransferHistory.java

--- src/main/java/org/example/model/TransferHistory.java
+++ src/main/java/org/example/model/TransferHistory.java
@@ -2,6 +2,7 @@ package org.example.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
@@ -16,12 +17,15 @@ public class TransferHistory {
     private int creditTransactionId;
     private double amount;
     private Date transferDate;
+    @Getter
     private int transactionDate;
 
-    public TransferHistory(int id, int debitTransactionId, int creditTransactionId, double amount, java.sql.Date date) {
-    }
 
-    public int getTransactionDate() {
-        return transactionDate;
+    public TransferHistory(int id, int debitTransactionId, int creditTransactionId, double amount, java.sql.Date transferDate) {
+        this.id= id;
+        this.debitTransactionId= debitTransactionId;
+        this.creditTransactionId=creditTransactionId;
+        this.amount=amount;
+        this.transferDate= transferDate;
     }
 }
