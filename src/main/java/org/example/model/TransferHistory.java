3115f64e7b41c371d92f2cbd4304fe877e400848
HarivolaANDY <hei.andy.43@gmail.com>
2026-07-06 20:43:54 +0300

    feat: initialize database schema, analytical functions, and TransferHistory model for wallet management system

--- src/main/java/org/example/model/TransferHistory.java
+++ src/main/java/org/example/model/TransferHistory.java
@@ -2,7 +2,6 @@ package org.example.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
@@ -10,22 +9,10 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class TransferHistory {
     private int id;
     private int debitTransactionId;
     private int creditTransactionId;
     private double amount;
     private Date transferDate;
-    @Getter
-    private int transactionDate;
-
-
-    public TransferHistory(int id, int debitTransactionId, int creditTransactionId, double amount, java.sql.Date transferDate) {
-        this.id= id;
-        this.debitTransactionId= debitTransactionId;
-        this.creditTransactionId=creditTransactionId;
-        this.amount=amount;
-        this.transferDate= transferDate;
-    }
 }
