50825cd46b372d8ee3a7cc2aac188138a35ffdb4
HarivolaANDY <hei.andy.43@gmail.com>
2026-07-06 20:44:36 +0300

    feat: add TransferHistory table schema and initial data migration script

--- src/main/resources/db/Create_table_transferHistory.sql
+++ src/main/resources/db/Create_table_transferHistory.sql
@@ -1,16 +1,15 @@
-CREATE TABLE IF NOT EXISTS transfertHistory(
+CREATE TABLE IF NOT EXISTS TransferHistory (
     id SERIAL PRIMARY KEY,
-    debitTransactionId INT REFERENCES transaction(id),
-    creditTransactionId INT REFERENCES transaction(id),
+    debitTransactionId INT REFERENCES transactions(id),
+    creditTransactionId INT REFERENCES transactions(id),
     transferDate TIMESTAMP NOT NULL,
     CONSTRAINT validation CHECK ( debitTransactionId IS NOT NULL AND creditTransactionId IS NOT NULL ),
     CONSTRAINT unicity UNIQUE (debitTransactionId, creditTransactionId, transferDate)
 );
 
-ALTER SEQUENCE transferHistory_id_seq RESTART WITH 1;
+ALTER SEQUENCE IF EXISTS transferhistory_id_seq RESTART WITH 1;
 
 INSERT INTO TransferHistory (debitTransactionId, creditTransactionId, transferDate) VALUES
-    (2, 1, '2023-01-02T12:00:00'),
+    (2, 1, '2023-01-02 12:00:00'),
     (1, 2, '2023-12-02 14:02:36'),
-    (3, 1, '2023-01-03T15:30:00');
-
+    (3, 1, '2023-01-03 15:30:00');
