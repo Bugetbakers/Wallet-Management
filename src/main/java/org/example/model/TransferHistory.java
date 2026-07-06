36fda7fd4bc8be47db49c61c9dda701d993fa913
HarivolaANDY <hei.andy.43@gmail.com>
2026-07-06 19:39:42 +0300

    feat: add TransferHistory model and unit tests for Account, Currency, and Transaction entities

--- src/main/java/org/example/model/TransferHistory.java
+++ src/main/java/org/example/model/TransferHistory.java
@@ -9,19 +9,10 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class TransferHistory {
     private int id;
     private int debitTransactionId;
     private int creditTransactionId;
     private double amount;
     private Date transferDate;
-    private int transactionDate;
-
-    public TransferHistory(int id, int debitTransactionId, int creditTransactionId, double amount, java.sql.Date date) {
-    }
-
-    public int getTransactionDate() {
-        return transactionDate;
-    }
 }
