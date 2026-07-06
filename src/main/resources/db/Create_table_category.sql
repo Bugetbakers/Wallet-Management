171cf9cee312707082d3fe18d803607d6370194d
HarivolaANDY <hei.andy.43@gmail.com>
2026-07-06 20:44:11 +0300

    feat: initialize database schema for exchange rates, categories, transactions, and transfer history

--- src/main/resources/db/Create_table_category.sql
+++ src/main/resources/db/Create_table_category.sql
@@ -1,12 +1,13 @@
-INSERT INTO categories (name) VALUES
-      ('Food and Drink'),
-      ('Transportation'),
-      ('Shopping'),
-      ('Health and Fitness'),
-      ('Travel'),
-      ('Education'),
-      ('Personal Care'),
-      ('Gifts and Donations'),
-      ('Home and Rent'),
-      ('Investments'),
-      ('Income');
+INSERT INTO transactionCategory (name, transaction_type) VALUES
+      ('Food and Drink', 'Output'),
+      ('Transportation', 'Output'),
+      ('Shopping', 'Output'),
+      ('Health and Fitness', 'Output'),
+      ('Travel', 'Output'),
+      ('Education', 'Output'),
+      ('Personal Care', 'Output'),
+      ('Gifts and Donations', 'Input and Output'),
+      ('Home and Rent', 'Output'),
+      ('Investments', 'Input and Output'),
+      ('Income', 'Input')
+ON CONFLICT (name) DO NOTHING;
