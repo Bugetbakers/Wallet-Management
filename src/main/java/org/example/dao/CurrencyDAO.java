adb3b176b9d6918286102ad6270f475a3dd7ee21
HarivolaANDY <hei.andy.43@gmail.com>
2026-07-06 20:16:15 +0300

    feat: implement DAO layers for Account, Currency, and Transaction with corresponding unit tests

--- src/main/java/org/example/dao/CurrencyDAO.java
+++ src/main/java/org/example/dao/CurrencyDAO.java
@@ -18,7 +18,7 @@ public class CurrencyDAO implements CrudOperation<Currency>{
     private CurrencyMapper currencyMapper = new CurrencyMapper();
 
     @Override
-    public List<Currency> findAll() throws SQLException {
+    public List<Currency> findAll() {
         List<Currency> currencies = new ArrayList<>();
         String sql = "SELECT * FROM currency";
 
@@ -35,42 +35,48 @@ public class CurrencyDAO implements CrudOperation<Currency>{
     public List<Currency> saveAll(List<Currency> toSave) {
         try {
             connection.setAutoCommit(false);
-            String sql = "INSERT INTO currency (id, code, name, country) VALUES (?,?,?,?);";
-            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            String sql = "INSERT INTO currency (code, name) VALUES (?,?);";
+            try (PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
                 for (Currency currency : toSave) {
-                    statement.setInt(1, currency.getId());
-                    statement.setString(2, currency.getCode());
-                    statement.setString(3, currency.getName());
-
+                    statement.setString(1, currency.getCode());
+                    statement.setString(2, currency.getName());
                     statement.addBatch();
                 }
                 statement.executeBatch();
+                ResultSet generatedKeys = statement.getGeneratedKeys();
+                int index = 0;
+                while (generatedKeys.next()) {
+                    toSave.get(index).setId(generatedKeys.getInt(1));
+                    index++;
+                }
                 connection.commit();
             } catch (SQLException e) {
                 connection.rollback();
-                throw new RuntimeException();
+                throw new RuntimeException(e);
             } finally {
                 connection.setAutoCommit(true);
             }
         } catch (SQLException e) {
-            throw new RuntimeException();
+            throw new RuntimeException(e);
         }
         return toSave;
     }
 
     @Override
     public Currency save(Currency toSave) {
-        String sql = "INSERT INTO currency (id, code, name, country) VALUES (?,?,?,?);";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, toSave.getId());
-            statement.setString(2, toSave.getCode());
-            statement.setString(3, toSave.getName());
-
+        String sql = "INSERT INTO currency (code, name) VALUES (?,?);";
+        try (PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, toSave.getCode());
+            statement.setString(2, toSave.getName());
             statement.executeUpdate();
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                toSave.setId(generatedKeys.getInt(1));
+            }
+            return toSave;
         } catch (SQLException e) {
-            throw new RuntimeException();
+            throw new RuntimeException(e);
         }
-        return null;
     }
 
     @Override
