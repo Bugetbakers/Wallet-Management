e81b9d1cbaa93ce4ab23d8a12192834b2f6ef2af
HarivolaANDY <hei.andy.43@gmail.com>
2026-07-06 20:42:56 +0300

    feat: implement core database schema, models, and DAO layer for accounts, transactions, and transfers

--- src/main/java/org/example/dao/CurrencyDAO.java
+++ src/main/java/org/example/dao/CurrencyDAO.java
@@ -1,14 +1,12 @@
 package org.example.dao;
 
 import org.example.model.Currency;
-
-import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class CurrencyDAO extends CrudOperationReflect<Currency>{
+public class CurrencyDAO extends CrudOperationReflect<Currency> {
     public CurrencyDAO(Connection connection){
         super(connection);
     }
@@ -20,7 +18,7 @@ public class CurrencyDAO extends CrudOperationReflect<Currency>{
 
     @Override
     protected String getInsertColumns() {
-        return ("id, code, name");
+        return "(id, code, name)";
     }
 
     @Override
@@ -29,19 +27,18 @@ public class CurrencyDAO extends CrudOperationReflect<Currency>{
     }
 
     @Override
-    protected Currency mapResultSetToObject(ResultSet resultSet) throws SQLException, IllegalAccessException {
-        Currency currency= new Currency();
-        Field[] fields= currency.getClass().getDeclaredFields();
-        for(Field field : fields){
-            field.setAccessible(true);
-            field.set(currency, resultSet.getObject(field.getName()));
-        }
+    protected Currency mapResultSetToObject(ResultSet resultSet) throws SQLException {
+        Currency currency = new Currency();
+        currency.setId(resultSet.getInt("id"));
+        currency.setCode(resultSet.getString("code"));
+        currency.setName(resultSet.getString("name"));
         return currency;
     }
 
     @Override
     protected void setPreparedStatementParameters(PreparedStatement preparedStatement, Currency object) throws SQLException {
         preparedStatement.setInt(1, object.getId());
-
+        preparedStatement.setString(2, object.getCode());
+        preparedStatement.setString(3, object.getName());
     }
 }
